c939609af8741998029c749358878ccb7ec81c9a
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-07-15 00:33:15 +0300

    updated components

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,12 @@
 import React from "react";
-import user from "../data/user";
+import Links from "./Links";
 function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <h3>Links</h3>
-      <p>{props.bio}</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <link href="{props.Links.linkedin}"/>
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
